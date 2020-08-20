e5c65faee13b12746cc41901116c775e5d1400af
happiguru <hguruman@gmail.com>
2020-08-20 19:53:04 +0400

    Implemented a class using initializer

---
+++ square2.rb
@@ -0,0 +1,21 @@
+class Square
+    attr_accessor :side_length
+    def initialize side_length = 0
+        @side_length = side_length
+    end
+
+    def area
+        @side_length * @side_length
+    end
+
+    def perimeter 
+        4 * @side_length
+    end
+end
+
+s1 = Square.new 4
+s2 = Square.new
+s2.side_length = 5
+
+puts "Area of s1 #{s1.area} sq units"
+puts "Perimeter of s2 is #{s2.perimeter} units"
