cc884441a2295dca584dd63534835d97d5467184
happiguru <hguruman@gmail.com>
2020-08-21 01:59:42 +0400

    Added function to calculate area and perimeter of a cicle

--- ruby_exercise.rb
+++ ruby_exercise.rb
@@ -9,3 +9,17 @@ require 'date'
 current_time = DateTime.now
 cur_time = current_time.strftime "%d/%m/%Y %H:%H"
 puts "Current Date and Time: "+cur_time
+
+time1 = Time.new
+puts "Current time : " + time1.inspect
+
+#compute circles parameter
+
+puts "Enter radius"
+radius = gets.chomp.to_i
+pi = 22/7
+perimeter = 2 * pi * radius
+area = pi * radius * radius
+
+puts "Perimeter of the circle is : #{perimeter}"
+puts "area is : #{area}"
